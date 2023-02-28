7b1fd023f7c1648cf8fb87cd2bb960ea41bdf35f
VolkerMoeller <v.moeller@t-online.de>
2023-02-28 12:16:23 +0100

    JSON-Array

---
+++ script.js
@@ -0,0 +1,37 @@
+let questions = [
+
+    {
+        "question": "Welches Land sieht auf der Landkarte aus wie ein Stiefel?",
+        "answer-1": "Italien",
+        "answer-2": "USA",
+        "answer-3": "China",
+        "answer-4": "Thailand",
+        "right-answer": 1
+    },
+    {
+        "question": "Nach wem wird eine Frisur benannt?",
+        "answer-1": "Zöpfern",
+        "answer-2": "Piss-Pottern",
+        "answer-3": "Wikingern",
+        "answer-4": "Irokesen",
+        "right-answer": 4
+    },
+    {
+        "question": "Was passt zusammen?",
+        "answer-1": "Fidel Castro und Jamaika",
+        "answer-2": "Fidel Castro und Kuba",
+        "answer-3": "Fidel Castro und Bolivien",
+        "answer-4": "Fidel Castro und Hawaii",
+        "right-answer": 2
+    },
+    {
+        "question": "Welche Insel besitzt die berühmten Steinskulpturen, die Moais genannt werden?",
+        "answer-1": "Fidschi",
+        "answer-2": "Osterinseln",
+        "answer-3": "Vanuatu",
+        "answer-4": "Galapagos",
+        "right-answer": 2
+    },
+
+];
+
