36a2e69796842de25c93cb75d812d4806887aec1
VolkerMoeller <v.moeller@t-online.de>
2023-03-03 22:28:08 +0100

    EndScreen Ready

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 let questions = [
-
-    {
-        "question": "Welches Land sieht auf der Landkarte aus wie ein Stiefel?",
-        "answer-1": "Italien",
-        "answer-2": "USA",
-        "answer-3": "China",
-        "answer-4": "Thailand",
-        "right-answer": 1
-    },
-    {
-        "question": "Nach wem wird eine Frisur benannt?",
-        "answer-1": "Zöpfern",
-        "answer-2": "Piss-Pottern",
-        "answer-3": "Wikingern",
-        "answer-4": "Irokesen",
-        "right-answer": 4
-    },
-    {
-        "question": "Was passt zusammen?",
-        "answer-1": "Fidel Castro und Jamaika",
-        "answer-2": "Fidel Castro und Kuba",
-        "answer-3": "Fidel Castro und Bolivien",
-        "answer-4": "Fidel Castro und Hawaii",
-        "right-answer": 2
-    },
-    {
-        "question": "Welche Insel besitzt die berühmten Steinskulpturen, die Moais genannt werden?",
-        "answer-1": "Fidschi",
-        "answer-2": "Osterinseln",
-        "answer-3": "Vanuatu",
-        "answer-4": "Galapagos",
-        "right-answer": 2
-
-    },
+// 
+    // {
+        // "question": "Welches Land sieht auf der Landkarte aus wie ein Stiefel?",
+        // "answer-1": "Italien",
+        // "answer-2": "USA",
+        // "answer-3": "China",
+        // "answer-4": "Thailand",
+        // "right-answer": 1
+    // },
+    // {
+        // "question": "Nach wem wird eine Frisur benannt?",
+        // "answer-1": "Zöpfern",
+        // "answer-2": "Piss-Pottern",
+        // "answer-3": "Wikingern",
+        // "answer-4": "Irokesen",
+        // "right-answer": 4
+    // },
+    // {
+        // "question": "Was passt zusammen?",
+        // "answer-1": "Fidel Castro und Jamaika",
+        // "answer-2": "Fidel Castro und Kuba",
+        // "answer-3": "Fidel Castro und Bolivien",
+        // "answer-4": "Fidel Castro und Hawaii",
+        // "right-answer": 2
+    // },
+    // {
+        // "question": "Welche Insel besitzt die berühmten Steinskulpturen, die Moais genannt werden?",
+        // "answer-1": "Fidschi",
+        // "answer-2": "Osterinseln",
+        // "answer-3": "Vanuatu",
+        // "answer-4": "Galapagos",
+        // "right-answer": 2
+// 
+    // },
     {
         "question": "Wie heisst die Hauptstadt der USA?",
         "answer-1": "Washington D.C.",
@@ -61,6 +61,7 @@ let questions = [
 
 let currentQuestion = 0;
 let counter = -1;
+let counterRight = 0;
 
 
 function init() {
@@ -73,7 +74,10 @@ function showQuestion() {
     counter++;
     updateProgress(counter);
     if (currentQuestion >= questions.length) {
-        // showEndScreen
+        document.getElementById('containerQuiz').classList.add('d-none');
+        document.getElementById('containerQuizEnd').classList.remove('d-none');
+        document.getElementById('countRightAnswers').innerHTML = counterRight;
+        document.getElementById('countTotalAnswers').innerHTML = questions.length;
     } else {
         let question = questions[currentQuestion];
         document.getElementById('question-number').innerHTML = currentQuestion + 1;
@@ -96,6 +100,7 @@ function answer(selection) {
 
     if (selectedQuestionNumber == question['right-answer']) {
         styleRightAnswer(selection, idOfListItemRightAnswer);
+        counterRight++;
     } else {
         styleWrongAnswer(selection, idOfCurrentListItem, idOfRightAnswer, idOfListItemRightAnswer);
     }
