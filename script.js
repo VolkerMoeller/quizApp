9f58d6d71989acd6fa4b4551f302ba0c82b6c92e
VolkerMoeller <v.moeller@t-online.de>
2023-03-06 15:54:25 +0100

    next responsive

--- script.js
+++ script.js
@@ -8,55 +8,55 @@ let questions = [
         "answer-4": "Thailand",
         "right-answer": 1
     },
-    // {
-    //     "question": "Nach wem wird eine Frisur benannt?",
-    //     "answer-1": "Zöpfern",
-    //     "answer-2": "Piss-Pottern",
-    //     "answer-3": "Wikingern",
-    //     "answer-4": "Irokesen",
-    //     "right-answer": 4
-    // },
-    // {
-    //     "question": "Was passt zusammen?",
-    //     "answer-1": "Fidel Castro und Jamaika",
-    //     "answer-2": "Fidel Castro und Kuba",
-    //     "answer-3": "Fidel Castro und Bolivien",
-    //     "answer-4": "Fidel Castro und Hawaii",
-    //     "right-answer": 2
-    // },
-    // {
-    //     "question": "Welche Insel besitzt die berühmten Steinskulpturen, die Moais genannt werden?",
-    //     "answer-1": "Fidschi",
-    //     "answer-2": "Osterinseln",
-    //     "answer-3": "Vanuatu",
-    //     "answer-4": "Galapagos",
-    //     "right-answer": 2
+    {
+        "question": "Nach wem wird eine Frisur benannt?",
+        "answer-1": "Zöpfern",
+        "answer-2": "Piss-Pottern",
+        "answer-3": "Wikingern",
+        "answer-4": "Irokesen",
+        "right-answer": 4
+    },
+    {
+        "question": "Was passt zusammen?",
+        "answer-1": "Fidel Castro und Jamaika",
+        "answer-2": "Fidel Castro und Kuba",
+        "answer-3": "Fidel Castro und Bolivien",
+        "answer-4": "Fidel Castro und Hawaii",
+        "right-answer": 2
+    },
+    {
+        "question": "Welche Insel besitzt die berühmten Steinskulpturen, die Moais genannt werden?",
+        "answer-1": "Fidschi",
+        "answer-2": "Osterinseln",
+        "answer-3": "Vanuatu",
+        "answer-4": "Galapagos",
+        "right-answer": 2
 
-    // },
-    // {
-    //     "question": "Wie heisst die Hauptstadt der USA?",
-    //     "answer-1": "Washington D.C.",
-    //     "answer-2": "New York",
-    //     "answer-3": "Los Angeles",
-    //     "answer-4": "Rocky Beach",
-    //     "right-answer": 1
-    // },
-    // {
-    //     "question": "Wie heisst ein Inselstaat im Pazifischen Ozean?",
-    //     "answer-1": "Marshallinseln",
-    //     "answer-2": "Sheriffinseln",
-    //     "answer-3": "Rangerinseln",
-    //     "answer-4": "Lieutenantinseln",
-    //     "right-answer": 1
-    // },
-    // {
-    //     "question": "Wie heisst Indiens wichtigster und längster Fluss??",
-    //     "answer-1": "Seine",
-    //     "answer-2": "Spree",
-    //     "answer-3": "Ganges",
-    //     "answer-4": "Amazonas",
-    //     "right-answer": 3
-    // }
+    },
+    {
+        "question": "Wie heisst die Hauptstadt der USA?",
+        "answer-1": "Washington D.C.",
+        "answer-2": "New York",
+        "answer-3": "Los Angeles",
+        "answer-4": "Rocky Beach",
+        "right-answer": 1
+    },
+    {
+        "question": "Wie heisst ein Inselstaat im Pazifischen Ozean?",
+        "answer-1": "Marshallinseln",
+        "answer-2": "Sheriffinseln",
+        "answer-3": "Rangerinseln",
+        "answer-4": "Lieutenantinseln",
+        "right-answer": 1
+    },
+    {
+        "question": "Wie heisst Indiens wichtigster und längster Fluss??",
+        "answer-1": "Seine",
+        "answer-2": "Spree",
+        "answer-3": "Ganges",
+        "answer-4": "Amazonas",
+        "right-answer": 3
+    }
 ];
 
 let currentQuestion = 0;
@@ -73,11 +73,13 @@ function init() {
     showQuestion();
 }
 
+
 function enableButtons() {
     for (i = 1; i < 5; i++) {
         document.getElementById(`answerb-${i}`).disabled = false;
     }
 }
+
 
 function disableButtons() {
     for (i = 1; i < 5; i++) {
@@ -89,43 +91,71 @@ function disableButtons() {
 function showQuestion() {
     counter++;
     updateProgress(counter);
-    if (currentQuestion >= questions.length) {
-        AUDIO_fanfare.play();
-        document.getElementById('containerQuiz').classList.add('d-none');
-        document.getElementById('containerQuizEnd').classList.remove('d-none');
-        document.getElementById('countRightAnswers').innerHTML = `&ensp;` + counterRight;
-        document.getElementById('countTotalAnswers').innerHTML = questions.length;
+    if (gameOver()) {
+        showEndScreen();
     } else {
-        let question = questions[currentQuestion];
-        document.getElementById('question-number').innerHTML = currentQuestion + 1;
-        document.getElementById('card-question').innerHTML = question['question'];
-        document.getElementById('answer-1').innerHTML = question['answer-1'];
-        document.getElementById('answer-2').innerHTML = question['answer-2'];
-        document.getElementById('answer-3').innerHTML = question['answer-3'];
-        document.getElementById('answer-4').innerHTML = question['answer-4'];
+        updateToNextQuestion();
     }
 }
 
+
+function gameOver() {
+    return currentQuestion >= questions.length;
+}
+
+function updateToNextQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+    document.getElementById('card-question').innerHTML = question['question'];
+    document.getElementById('answer-1').innerHTML = question['answer-1'];
+    document.getElementById('answer-2').innerHTML = question['answer-2'];
+    document.getElementById('answer-3').innerHTML = question['answer-3'];
+    document.getElementById('answer-4').innerHTML = question['answer-4'];
+}
+
+function showEndScreen() {
+    AUDIO_fanfare.play();
+    document.getElementById('containerQuiz').classList.add('d-none');
+    document.getElementById('containerQuizEnd').classList.remove('d-none');
+    document.getElementById('countRightAnswers').innerHTML = `&ensp;` + counterRight;
+    document.getElementById('countTotalAnswers').innerHTML = questions.length;
+}
 
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
     let idOfCurrentListItem = `listItem-${selectedQuestionNumber}`;
-
     let idOfRightAnswer = `answer-${question['right-answer']}`;
     let idOfListItemRightAnswer = `listItem-${question['right-answer']}`
-
-    if (selectedQuestionNumber == question['right-answer']) {
-        AUDIO_success.play();
-        styleRightAnswer(selection, idOfListItemRightAnswer);
-        disableButtons();
-        counterRight++;
+    if (answerWasCorrect(selectedQuestionNumber, question)) {
+        animateButtonAndUpdateCounter(selection, idOfListItemRightAnswer);
     } else {
-        AUDIO_wrong.play();
-        styleWrongAnswer(selection, idOfCurrentListItem, idOfRightAnswer, idOfListItemRightAnswer);
-        disableButtons();
+        animateRightAndWrongButton(selection, idOfCurrentListItem, idOfRightAnswer, idOfListItemRightAnswer);
     }
+    enableNextButton();
+}
+
+
+function enableNextButton() {
     document.getElementById('next-Button').disabled = false;
+}
+
+
+function animateButtonAndUpdateCounter(selection, idOfListItemRightAnswer) {
+    AUDIO_success.play();
+    styleRightAnswer(selection, idOfListItemRightAnswer);
+    disableButtons();
+    counterRight++;
+}
+
+function animateRightAndWrongButton(selection, idOfCurrentListItem, idOfRightAnswer, idOfListItemRightAnswer) {
+    AUDIO_wrong.play();
+    styleWrongAnswer(selection, idOfCurrentListItem, idOfRightAnswer, idOfListItemRightAnswer);
+    disableButtons();
+}
+
+function answerWasCorrect(selectedQuestionNumber, question) {
+    return selectedQuestionNumber == question['right-answer'];
 }
 
 
@@ -135,6 +165,7 @@ function styleRightAnswer(selection, idOfListItemRightAnswer) {
     document.getElementById(idOfListItemRightAnswer).classList.add('bg-success');
     document.getElementById(idOfListItemRightAnswer).classList.add('text-white');
 }
+
 
 function styleWrongAnswer(selection, idOfCurrentListItem, idOfRightAnswer, idOfListItemRightAnswer) {
     document.getElementById(selection).parentNode.classList.add('bg-danger');
@@ -147,17 +178,24 @@ function styleWrongAnswer(selection, idOfCurrentListItem, idOfRightAnswer, idOfL
     document.getElementById(idOfListItemRightAnswer).classList.add('text-white');
 }
 
+
 function nextQuestion() {
+    stopSound();
     currentQuestion++;
     document.getElementById('next-Button').disabled = true;
     resetAnswerButtons();
     showQuestion();
     enableButtons();
+}
+
+
+function stopSound() {
     AUDIO_success.pause();
     AUDIO_success.currentTime = 0;
     AUDIO_wrong.pause();
     AUDIO_wrong.currentTime = 0;
 }
+
 
 function resetAnswerButtons() {
     for (let i = 1; i < 5; i++) {
@@ -170,18 +208,31 @@ function resetAnswerButtons() {
     }
 }
 
+
 function updateProgress(counter) {
     let valueProgress = 100 / questions.length * counter;
     let newProgress = `width: ${valueProgress}%`;
     document.getElementById('progress').style = newProgress;
 }
 
+
 function replay() {
+    stopSound();
+    setGlobalValues();
+    hideEndScreenAndShowQuiz();
+    enableButtons();
+    init();
+}
+
+
+function hideEndScreenAndShowQuiz() {
     document.getElementById('containerQuizEnd').classList.add('d-none');
     document.getElementById('containerQuiz').classList.remove('d-none');
+}
+
+
+function setGlobalValues() {
     currentQuestion = 0;
     counter = -1;
     counterRight = 0;
-    enableButtons();
-    init();
 }
